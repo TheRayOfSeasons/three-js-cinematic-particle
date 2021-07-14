2841f4e17828df85b5ec2a7734f21aa7d9833847
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-07-14 13:30:32 +0800

    Draft: detailed neuron

--- src/pages/TestPage10.js
+++ src/pages/TestPage10.js
@@ -16,14 +16,15 @@ const NeuronAnimation = canvas => {
       this.renderer = CORE.createRenderer(canvas, canvasWidth, canvasHeight);
       this.scene = CORE.createScene();
       this.camera = CORE.createCamera(canvasWidth, canvasHeight);
-      this.camera.position.z = 2;
+      this.camera.position.z = 1;
 
       const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-      hemiLight.position.set( 0, 200, 0 );
+      hemiLight.position.set( 100, 200, 0 );
       this.scene.add( hemiLight );
 
       const dirLight = new THREE.DirectionalLight( 0xffffff );
-      dirLight.position.set( 0, 200, 100 );
+      dirLight.intensity = 0.25;
+      dirLight.position.set( 75, 200, 100 );
       dirLight.castShadow = true;
       dirLight.shadow.camera.top = 180;
       dirLight.shadow.camera.bottom = - 100;
@@ -31,9 +32,48 @@ const NeuronAnimation = canvas => {
       dirLight.shadow.camera.right = 120;
       this.scene.add( dirLight );
 
-      this.neuron = createAnimatedNeuron();
-      this.neuron.init();
-      this.scene.add(this.neuron.group);
+      this.neuron1 = createAnimatedNeuron();
+      this.neuron1.init();
+      this.scene.add(this.neuron1.group);
+      this.neuron1.group.rotation.x = -Math.PI * 0.075;
+      this.neuron1.group.rotation.y = -Math.PI;
+      this.neuron1.group.rotation.z = -Math.PI * 0.05;
+      this.neuron1.group.position.x = 0.75;
+      
+      this.neurons = [
+        (() => {
+          const neuron = createAnimatedNeuron();
+          neuron.init();
+          this.scene.add(neuron.group);
+          neuron.group.rotation.x = -Math.PI * 0.075;
+          neuron.group.rotation.y = -Math.PI;
+          neuron.group.rotation.z = -Math.PI * 0.05;
+          neuron.group.position.x = 0.75;
+          return neuron;
+        })(),
+        (() => {
+          const neuron = createAnimatedNeuron();
+          neuron.init();
+          this.scene.add(neuron.group);
+          neuron.group.rotation.x = -Math.PI * 0.075;
+          neuron.group.rotation.y = Math.PI * 0.35;
+          neuron.group.position.x = -1.75;
+          neuron.group.position.z = -2;
+          return neuron;
+        })(),
+        (() => {
+          const neuron = createAnimatedNeuron();
+          neuron.init();
+          this.scene.add(neuron.group);
+          neuron.group.rotation.x = -Math.PI * 0.075;
+          neuron.group.rotation.y = Math.PI * 0.75;
+          neuron.group.rotation.z = Math.PI * 0.25;
+          neuron.group.position.x = 3;
+          neuron.group.position.y = 1.75;
+          neuron.group.position.z = -3;
+          return neuron;
+        })()
+      ];
 
       console.log('Initialization done!');
       this.renderer.setAnimationLoop(this.update());
@@ -46,8 +86,10 @@ const NeuronAnimation = canvas => {
       console.log('Begining animation...');
       return time => {
         stats.begin();
-        this.neuron.update(time);
         this.controls.update();
+        for(const neuron of this.neurons) {
+          neuron.update(time);
+        }
         this.renderer.render(this.scene, this.camera);
         stats.end();
       }
@@ -63,12 +105,14 @@ export const TestPage10 = () => {
     if(canvasScene) {
       let animation = NeuronAnimation(canvasScene.current);
       animation.init();
+      document.getElementById('test-stats').appendChild(stats.dom);
     }
   }, [canvasScene]);
 
   return (
     <div className="hero-container">
       <div id="test-stats"></div>
+      <div class="dirty-white-background"></div>
       <canvas ref={canvasScene} className="blurred" style={{
         width: '100vw',
         height: '100vh',
